330be97e165c8dade4da0351590e818efd28dbe8
Brandon Balbach <bwbalbach@gmail.com>
2021-08-10 20:23:08 -0400

    Initial commit - database setup

---
+++ src/main/java/com/brandon/contactsdatabase/ContactsdatabaseApplication.java
@@ -0,0 +1,13 @@
+package com.brandon.contactsdatabase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContactsdatabaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ContactsdatabaseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/brandon/contactsdatabase/ContactsdatabaseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.brandon.contactsdatabase;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ContactsdatabaseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
